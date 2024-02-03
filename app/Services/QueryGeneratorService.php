4466c4bc77698f6d0fc7b801ef97eb11e21cc369
Ben Todd <happynowbtodd@gmail.com>
2024-02-03 17:10:09 -0600

    All queries back

--- app/Services/QueryGeneratorService.php
+++ app/Services/QueryGeneratorService.php
@@ -12,12 +12,12 @@ class QueryGeneratorService
             'content-type' => 'application/json',
             'origin' => 'https://tmsearch.uspto.gov',
             'referer' => 'https://tmsearch.uspto.gov/search/search-results',
-           // 'sec-ch-ua' => '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-           // 'sec-ch-ua-mobile' => '?0',
-//            'sec-ch-ua-platform' => '"macOS"',
-//            'sec-fetch-dest' => 'empty',
-//            'sec-fetch-mode' => 'cors',
-//            'sec-fetch-site' => 'same-origin',
+            'sec-ch-ua' => '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+            'sec-ch-ua-mobile' => '?0',
+            'sec-ch-ua-platform' => '"macOS"',
+            'sec-fetch-dest' => 'empty',
+            'sec-fetch-mode' => 'cors',
+            'sec-fetch-site' => 'same-origin',
             'user-agent' => 'Mozilla/5.0 (compatible; Monogram/1.0; https://monogramlegal.com/)'
        //   'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         ];
