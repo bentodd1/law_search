2e2f709faaf91b23c2224060af4aad7b45f4ae2d
Ben Todd <happynowbtodd@gmail.com>
2024-02-06 15:53:53 -0600

    clear out

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Services\QueryGeneratorService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 class SearchController extends Controller
 {
