7d5973ddad6cc3b89ab5aaf9b1629e2b01279eba
Ben Todd <happynowbtodd@gmail.com>
2024-03-24 15:59:08 -0500

    Change to status

--- resources/views/cases/show.blade.php
+++ resources/views/cases/show.blade.php
@@ -5,11 +5,11 @@
             <h1 class="text-2xl font-bold mb-4">Case Details</h1>
 
             <p><strong>Serial Number:</strong> {{ $case->serial_number }}</p>
-            <p><strong>Mark Identification:</strong> {{ $case->mark_identification }}</p>
+            <p><strong>Mark Identification:</strong> {{$case->header->mark_identification }}</p>
             <p><strong>USPTO Link:</strong> <a href="https://tsdr.uspto.gov/#caseNumber={{ $case->serial_number }}&caseSearchType=US_APPLICATION&caseType=DEFAULT&searchType=statusSearch" target="_blank" class="text-blue-600 hover:text-blue-800">View on USPTO</a></p>
             <p><strong>Contains 2(d):</strong> {{ $case->contains_2d ? 'Yes' : 'No' }}</p>
             <!--<p><strong>Overcame Rejection:</strong> {{ $case->overcame_rejection ? 'Yes' : 'No' }}</p> -->
-            <p><strong>Status:</strong> {{ $case->status }}</p>
+            <p><strong>Status:</strong> {{ $case->header->status }}</p>
 
             <!-- Back to search results or other navigation link -->
         </div>
